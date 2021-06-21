8035895c32ebfe6b7db74973d9808e51ed45c896
Gustavo <f170015@gmail.com>
2021-06-20 21:30:33 -0500

    iniciando la app de musica

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  optionsSlides = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centeredSlides: true,
+    speed: 500,
+  };
+  constructor() {}
+}
